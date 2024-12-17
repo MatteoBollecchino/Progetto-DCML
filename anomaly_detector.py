ef3b6986ca4fd1b670338aaa3d65f62f0e2b2fe0
MatteoBollecchino <matthbolle@gmail.com>
2024-12-17 13:08:36 +0100

    Update anomaly_detector.py

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -14,30 +14,15 @@ from sklearn.tree import DecisionTreeClassifier
 # Sets random seed to increase repeatability
 random.seed(23)
 
-
-def current_ms() -> int:
-    """
-    Reports the current time in milliseconds
-    :return: long int
-    """
-    return round(time.time() * 1000)
-
-
 if __name__ == "__main__":
-    """
-    Main of the data analysis
-    """
 
-    # load dataset PANDAS / NUMPY
+    # load dataset
     my_dataset = read_csv("labelled_dataset.csv")
     label_obj = my_dataset["label"]
     data_obj = my_dataset.drop(columns=["label", "time", "datetime"])
-    # the row above is "equivalent" to
-    # my_dataset.drop(columns=["label"], inplace=True)
 
     # split dataset
-    train_data, test_data, train_label, test_label = \
-        train_test_split(data_obj, label_obj, test_size=0.5)
+    train_data, test_data, train_label, test_label = train_test_split(data_obj, label_obj, test_size=0.5)
 
     # choose classifier SCIKIT LEARN
     # Set of classifiers that I want to run and compare
@@ -58,16 +43,13 @@ if __name__ == "__main__":
 
     for clf in classifiers:
         # Training an algorithm
-        before_train = current_ms()
         clf = clf.fit(train_data, train_label)
-        after_train = current_ms()
 
         # Testing the trained model
         predicted_labels = clf.predict(test_data)
-        end = current_ms()
 
         # Computing metrics to understand how good an algorithm is
         accuracy = sklearn.metrics.accuracy_score(test_label, predicted_labels)
         tn, fp, fn, tp = confusion_matrix(test_label, predicted_labels).ravel()
         print("%s: Accuracy is %.4f, train time: %d, test time: %d TP: %d, TN: %d, FN: %d, FP: %d" % (
-            clf.__class__.__name__, accuracy, after_train - before_train, end - after_train, tp, tn, fn, fp))
+            clf.__class__.__name__, accuracy, tp, tn, fn, fp))
